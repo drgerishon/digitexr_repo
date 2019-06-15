b52fc7fee00bf37aa1cde2f1d0979cfcaad77c8b
Galavu <Bond>
2019-06-15 15:27:47 +0300

    base1

--- digi/settings.py
+++ digi/settings.py
@@ -38,6 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'accounts.apps.AccountsConfig',
+
+
+
+
 ]
 
 MIDDLEWARE = [
@@ -123,3 +127,4 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
