f3887993620ca6c610ced555073967d13e589d26
bond50 <43951946+bond50@users.noreply.github.com>
2019-06-16 16:47:08 +0300

    Update settings.py

--- digi/settings.py
+++ digi/settings.py
@@ -75,14 +75,20 @@ WSGI_APPLICATION = 'digi.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'digitex',
+#         'USER': 'postgres',
+#         'PASSWORD': '22741845',
+#         'HOST': '127.0.0.1',
+#         'PORT': '5432',
+#     }
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'digitex',
-        'USER': 'postgres',
-        'PASSWORD': '22741845',
-        'HOST': '127.0.0.1',
-        'PORT': '5432',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
 # Password validation
